4781407e1bc31fd6313dc8ac1a992b8fa664d076
GlitchGod42 <tobikmotyka54@gmail.com>
2026-03-07 20:57:03 +0100

    Added QOLMod
    thank you bobisbilly for helping dude holy fuck 36 files

---
+++ QOLMod/Main.cs
@@ -0,0 +1,34 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+
+
+namespace QOLMod
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HelloWorld();
+        }
+
+        public static void HelloWorld()
+        {
+            MelonLogger.Msg("herro from QOLMod");
+        }
+
+        [HarmonyPatch(typeof(PlayerReferenceManager), nameof(PlayerReferenceManager.OnPlayerReferenceAdded))]
+        public static class AnyName
+        {
+            [HarmonyPostfix]
+            public static void PostFix(int index)
+            {
+                LobbySettings lobbySettings = LobbySettingsManager.Instance.GetLobbySettings();
+
+                lobbySettings.maxBuildableItems = 2147483647;
+
+                PlayerReferenceManager.Instance.GetLocalPlayerReference().PlayerControl.buildingController.OnLobbySettingsChanged(lobbySettings);
+            }
+        }
+    }
+}
---
+++ QOLMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(QOLMod.Main), "QOLMod", "1.0.0", "GlitchGod42, thanks to BobisBilly for fixing")]
+[assembly: MelonGame("The Sledding Corporation", null)]
